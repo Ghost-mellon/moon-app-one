8b80b6a208961cf16c1c27a92b31113e82c49b14
Ghost-mellon <cybermonk369@gmail.com>
2023-12-04 12:04:41 +0530

    commit with working material sidebar-dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import ColorInversionHeader from './components/Navbar';
-import SvgIconsColor from './components/Navbar';
+// import SvgIconsColor from './components/Navbar';
+// import NavbarMaterial from './components/NavbarMaterial';
 
 function App() {
   return (
     <div className="App">
       <ColorInversionHeader/>
+      {/* <NavbarMaterial/> */}
     </div>
   );
 }
